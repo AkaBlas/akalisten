beef84bacbbf2ddff2f10a88711fa5fda40369e2
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-11-10 22:25:18 +0100

    Improve Debug Mode

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ load_dotenv(override=True)
 
 
 ROOT = Path(__file__).parent
+DUMMY_DATA = ROOT / "dummy_data.json"
 LOGS = ROOT / "logs"
 LOGS.mkdir(exist_ok=True)
+
+DEBUG_MODE = os.getenv("DEBUG") is not None
 
 handler = TimedRotatingFileHandler(
     filename=LOGS / "akalisten.log", when="midnight", interval=1, backupCount=30, encoding="utf-8"
@@ -29,33 +32,32 @@ handler = TimedRotatingFileHandler(
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.WARNING,
-    handlers=[handler],
+    handlers=[handler] if not DEBUG_MODE else [logging.StreamHandler()],
 )
 
 
 async def get_poll_data() -> tuple[dict[int, Poll], dict[int, PollVotes]]:
-    class TestData(BaseModel):
+    class TempData(BaseModel):
         polls: dict[int, Poll]
         poll_votes: dict[int, PollVotes]
 
-    if os.getenv("DUMMY_DATA") is not None:
-        test_data = TestData.model_validate_json(
-            Path("dummy_data.json").read_text(encoding="utf-8")
-        )
+    if DEBUG_MODE and DUMMY_DATA.exists():
+        tmp_data = TempData.model_validate_json(DUMMY_DATA.read_text(encoding="utf-8"))
     else:
-        test_data = TestData(polls={}, poll_votes={})
+        tmp_data = TempData(polls={}, poll_votes={})
 
         async with PollAPI() as client:
             for poll in await client.get_polls():
                 if not poll.is_mucken_liste:
                     continue
                 votes = await client.aggregate_poll_votes(poll.id)
-                test_data.poll_votes[poll.id] = votes
-                test_data.polls[poll.id] = poll
+                tmp_data.poll_votes[poll.id] = votes
+                tmp_data.polls[poll.id] = poll
 
-        # Path("dummy_data.json").write_text(test_data.model_dump_json(indent=2), encoding="utf-8")
+        if DEBUG_MODE:
+            DUMMY_DATA.write_text(tmp_data.model_dump_json(indent=2), encoding="utf-8")
 
-    return test_data.polls, test_data.poll_votes
+    return tmp_data.polls, tmp_data.poll_votes
 
 
 async def main() -> None:
