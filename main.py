910fca5171fb1f3c950a55c9ab6f591631b86b40
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-11-10 21:43:42 +0100

    Add some exception handling logging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import datetime as dtm
+import logging
 import zoneinfo
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -12,6 +14,21 @@ from akalisten.clients.polls import PollAPI
 from akalisten.polls import PollVotes
 
 load_dotenv(override=True)
+
+
+ROOT = Path(__file__).parent
+LOGS = ROOT / "logs"
+LOGS.mkdir(exist_ok=True)
+
+handler = TimedRotatingFileHandler(
+    filename=LOGS / "akalisten.log", when="midnight", interval=1, backupCount=30, encoding="utf-8"
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.WARNING,
+    handlers=[handler],
+)
 
 
 async def get_poll_data() -> tuple[dict[int, Poll], dict[int, PollVotes]]:
@@ -39,15 +56,16 @@ async def get_poll_data() -> tuple[dict[int, Poll], dict[int, PollVotes]]:
 async def main() -> None:
     polls, poll_votes = await get_poll_data()
 
-    root = Path(__file__).parent
-
     template = Template(
-        (root / Path("akalisten/templates/hinrich.j2")).read_text(encoding="utf-8")
+        (ROOT / Path("akalisten/templates/hinrich.j2")).read_text(encoding="utf-8")
     )
     timezone = zoneinfo.ZoneInfo("Europe/Berlin")
     out = template.render(polls=polls, poll_votes=poll_votes, now=dtm.datetime.now(timezone))
-    (root / "index.html").write_text(out, encoding="utf-8")
+    (ROOT / "index.html").write_text(out, encoding="utf-8")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as exc:
+        logging.exception(exc)
