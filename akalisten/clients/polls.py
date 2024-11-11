cc08cd2840e4fa48bdaacc5dbc9c4fa3c2e35036
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-11-11 18:11:29 +0100

    More Descriptive Error Handling for API Requests

--- akalisten/clients/polls.py
+++ akalisten/clients/polls.py
@@ -1,6 +1,6 @@
 import os
-from collections.abc import Sequence
-from contextlib import AbstractAsyncContextManager
+from collections.abc import Iterator, Sequence
+from contextlib import AbstractAsyncContextManager, contextmanager
 from types import TracebackType
 from typing import Optional
 
@@ -8,6 +8,18 @@ import httpx
 
 from akalisten.api_types.polls import Poll, PollOption, PollVote
 from akalisten.polls import PollVotes
+
+
+@contextmanager
+def _response_handler(response: httpx.Response) -> Iterator[None]:
+    response.raise_for_status()
+    try:
+        yield None
+    except Exception as exc:
+        raise RuntimeError(
+            f"Failed to parse API response `{response.content!r}` with status "
+            f"`{response.status_code}`."
+        ) from exc
 
 
 class PollAPI(AbstractAsyncContextManager):
@@ -30,19 +42,23 @@ class PollAPI(AbstractAsyncContextManager):
 
     async def get_polls(self) -> Sequence[Poll]:
         response = await self.client.get(self.base_url + "polls")
-        return [Poll(**poll) for poll in response.json()["polls"]]
+        with _response_handler(response):
+            return [Poll(**poll) for poll in response.json()["polls"]]
 
     async def get_poll(self, poll_id: int) -> Poll:
         response = await self.client.get(self.base_url + f"poll/{poll_id}")
-        return Poll(**response.json())
+        with _response_handler(response):
+            return Poll(**response.json())
 
     async def get_poll_options(self, poll_id: int) -> Sequence[PollOption]:
         response = await self.client.get(self.base_url + f"poll/{poll_id}/options")
-        return [PollOption(**option) for option in response.json()["options"]]
+        with _response_handler(response):
+            return [PollOption(**option) for option in response.json()["options"]]
 
     async def get_poll_votes(self, poll_id: int) -> Sequence[PollVote]:
         response = await self.client.get(self.base_url + f"poll/{poll_id}/votes")
-        return [PollVote(**vote) for vote in response.json()["votes"]]
+        with _response_handler(response):
+            return [PollVote(**vote) for vote in response.json()["votes"]]
 
     async def aggregate_poll_votes(self, poll_id: int) -> PollVotes:
         votes = await self.get_poll_votes(poll_id)
