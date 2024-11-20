00f1eba2971ac936152d7044600f7ba5523297b8
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-11-20 22:57:51 +0100

    Display Names as "First L.". Close #9

--- akalisten/polls.py
+++ akalisten/polls.py
@@ -203,7 +203,11 @@ class User(BaseModel):
 
     @property
     def display_name(self) -> str:
-        return ", ".join(self.name.rsplit(" ", 1)[::-1]) if " " in self.name else self.name
+        if " " not in self.name:
+            return self.name
+        names = self.name.split(" ")
+        # in case the last name has several parts, we take the first letter of each part
+        return f"{names[0]} {''.join(part[0] for part in names[1:])}."
 
     @property
     def html_display_name(self) -> str:
